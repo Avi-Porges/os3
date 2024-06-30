51e1af718d05a658d77e755a6ba5e43318fab803
avi.porges <Avi.Porges@mail.huji.ac.il>
2024-06-30 18:25:06 +0300

    c

---
+++ MapReduceFramework.cpp
@@ -0,0 +1,24 @@
+
+struct ThreadContext {
+
+
+};
+
+
+
+
+
+
+JobHandle startMapReduceJob(const MapReduceClient& client,
+                            const InputVec& inputVec, OutputVec& outputVec,
+                            int multiThreadLevel){
+
+
+
+
+
+}
+
+
+
+
